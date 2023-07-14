18526bf54930c1c960a3ce867abdefbc9f5e3553
Chris Anselmo <christopherwk210@gmail.com>
2023-07-14 09:34:50 -0700

    change tool window controls into icon buttons

--- src/components/ToolWindow.vue
+++ src/components/ToolWindow.vue
@@ -12,9 +12,13 @@ const props = defineProps<{
       <div class="card-header bg-primary no-text d-flex">
         {{ props.title }}
 
-        <div class="ms-auto">
-          <a v-if="props.aboutLink" :href="props.aboutLink" target="_blank" class="btn-link text-dark ms-auto me-3">About</a>
-          <RouterLink v-if="props.backRoute" :to="props.backRoute" class="btn-link text-dark ms-auto">Back</RouterLink>
+        <div class="controls">
+          <a v-if="props.aboutLink" :href="props.aboutLink" target="_blank" class="btn-link text-dark">
+            <i class="bi-question-circle-fill"></i>
+          </a>
+          <RouterLink v-if="props.backRoute" :to="props.backRoute" class="btn-link text-dark">
+            <i class="bi-arrow-left"></i>
+          </RouterLink>
         </div>
       </div>
       <div class="card-body bg-secondary p-0">
@@ -23,3 +27,32 @@ const props = defineProps<{
     </div>
   </div>
 </template>
+
+<style scoped>
+  .card-header {
+    position: relative;
+  }
+
+  .controls {
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    display: flex;
+  }
+
+  .btn-link {
+    height: 100%;
+    font-size: 18px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    width: 40px;
+  }
+
+  .btn-link:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+    color: white !important;
+  }
+</style>
